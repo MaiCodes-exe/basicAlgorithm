b1de627d5a35a25a4e9b91550d4f8df1d54dfa76
MaiCodes-exe <maise.b.costa@gmail.com>
2023-03-24 15:09:49 +0000

    6th need to review splice method

--- 6.js
+++ 6.js
@@ -1,1 +1,7 @@
 // Confirm the Ending
+function confirmEnding(str, target) {
+
+  return str.slice(str.length - target.length) === target;
+}
+
+confirmEnding("He has to give me a new name", "name");
