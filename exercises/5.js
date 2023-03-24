c661fb8c3fb2d486bf1eda5cd2d10a21e8e764e2
MaiCodes-exe <maise.b.costa@gmail.com>
2023-03-24 14:49:13 +0000

    5th solved

--- exercises/5.js
+++ exercises/5.js
@@ -1,1 +1,30 @@
 // Return Largest Numbers in Arrays
+
+// First attempt
+// function largestOfFour(arr) {
+//   let results = [];
+//   for (let i = 0 ; i < arr.length ; i++){
+// const currentArray = arr[i].sort().reverse();
+// results.push(currentArray[0])
+//   }
+// console.log(results)
+//   return results;
+  
+
+// }
+
+
+function largestOfFour(arr) {
+
+  var arrNew = [];
+
+  for(var i = 0; i < arr.length; i++){
+    arrNew.push(arr[i].sort(function(a, b){
+      return b-a;
+    })[0]);
+  }
+
+  return arrNew;
+
+ }
+ largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
