2c8c2b8f2f81809f7d5951449c5c8c42fd74ce5b
MaiCodes-exe <maise.b.costa@gmail.com>
2023-03-24 21:14:22 +0000

    8th done

--- exercises/8.js
+++ exercises/8.js
@@ -1,3 +1,13 @@
 // Truncate a String
 
 
+function truncateString(str, num) {
+  // Clear out that junk in your trunk
+  if (str.length > num) {
+    return str.slice(0, num) + "...";
+  } else {
+    return str;
+  }
+}
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
