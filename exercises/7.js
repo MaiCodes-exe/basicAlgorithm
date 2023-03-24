47049a1909b13f4dccb037b9ed82bcab6c425400
MaiCodes-exe <maise.b.costa@gmail.com>
2023-03-24 15:24:19 +0000

    7th done

--- exercises/7.js
+++ exercises/7.js
@@ -1,1 +1,13 @@
 // Repeat a String Repeat a String
+function repeatStringNumTimes(str, num) {
+  if (num <= 0){
+    return "";
+  }
+  else{
+    return str  + repeatStringNumTimes(str, num - 1);
+  }
+
+  // return str;
+}
+
+repeatStringNumTimes("abc", 3);
