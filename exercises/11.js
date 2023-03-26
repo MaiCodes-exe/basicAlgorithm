a1ae51b9dca9361cb9532d74ae69dbcba3556c9d
MaiCodes-exe <maise.b.costa@gmail.com>
2023-03-26 19:37:50 +0000

    11th need to review touppercase and tolowercase

--- exercises/11.js
+++ exercises/11.js
@@ -1,2 +1,15 @@
 // Title Case a Sentence
 
+function titleCase(str) {
+  str = str.split(' ');
+
+  for (let i = 0; i < str.length; i++) {
+    str[i] = str[i].toLowerCase().split('');
+    str[i][0] = str[i][0].toUpperCase();
+    str[i] = str[i].join('');
+  }
+
+  return str.join(' ');
+}
+
+titleCase("I'm a Little tea pot");
