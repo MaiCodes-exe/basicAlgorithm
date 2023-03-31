9a51469ca7b45a8c12d01ba8c03b62ef3fa6ee8e
MaiCodes-exe <maise.b.costa@gmail.com>
2023-03-31 20:40:57 +0000

    15th done, need to review  toLowercase

--- exercises/15.js
+++ exercises/15.js
@@ -1,2 +1,10 @@
 // Mutations
 
+function mutation(arr) {
+  return arr[1]
+    .toLowerCase()
+    .split("")
+    .every(function(letter) {
+      return arr[0].toLowerCase().indexOf(letter) !== -1;
+    });
+}
